8e74c9bd505e1253d121e26480b8470123597a72
Gareth Brown <gareth@mesoform.com>
2022-11-08 11:36:03 +0000

    output list of Cloud Run service IDs

--- gcp_outputs.tf
+++ gcp_outputs.tf
@@ -1,19 +1,22 @@
-output "gae" {
+output gae {
   value = local.gae
 }
 
-output "gae_flex" {
+output gae_flex {
   value = local.as_flex_specs
 }
 
-output "gae_std" {
+output gae_std {
   value = local.as_std_specs
 }
 
-output "cloudrun" {
+output cloudrun {
   value = local.cloudrun
 }
 
-output "cloudrun_service" {
-  value = values(local.cloudrun_specs).*.name
+output cloudrun_service_ids {
+  value = [
+    for service in google_cloud_run_service.self:
+          service.id
+  ]
 }
