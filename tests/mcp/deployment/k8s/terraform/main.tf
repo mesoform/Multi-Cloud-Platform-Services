deda3df38dc8de768f3dc36af67698f5e280cdf8
dan <dan@mesoform.com>
2025-10-12 20:21:47 +0200

    Updates python runtime version to a currently supported version on App Engine configuration; Moves kubernetes provider block from shared main.tf file to dedicated Kuberentes directory's main.tf file

--- tests/mcp/deployment/k8s/terraform/main.tf
+++ tests/mcp/deployment/k8s/terraform/main.tf
@@ -5,6 +5,10 @@ terraform{
   }
 }
 
+provider "kubernetes" {
+  load_config_file = true
+}
+
 module "mcp" {
   source = "../../../../../"
 #  source = "source-path"
--- tests/terraform/main.tf
+++ tests/terraform/main.tf
@@ -1,7 +1,3 @@
-provider "kubernetes" {
-  load_config_file = true
-}
-
 module "mcp" {
   source  = "../../mcp"
 }
