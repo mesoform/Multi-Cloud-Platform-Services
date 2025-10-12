68fcfc584229eec2631c24236e69b79216884e60
dan <dan@mesoform.com>
2025-10-13 00:51:01 +0200

    Updates python runtime api version to a currently supported version on App Engine configuration; Add temporary Kubernetes provider configuration to prevent the config_path error during "Deploy GCP modules" step

--- tests/terraform/main.tf
+++ tests/terraform/main.tf
@@ -1,3 +1,11 @@
+provider "kubernetes" {
+  host                   = "dummy-host"
+  token                  = "dummy-token"
+  cluster_ca_certificate = "dummy-cert"
+  insecure               = true
+  load_config_file       = false
+}
+
 module "mcp" {
   source  = "../../mcp"
 }
