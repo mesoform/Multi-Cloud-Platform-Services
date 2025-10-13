a19580ac46d107dd6405f7a06f38d65a57023d37
dan <dan@mesoform.com>
2025-10-13 12:28:18 +0200

    Removes unsupported load_config_file argument from kubernetes provider

--- tests/mcp/deployment/GCP/terraform/main.tf
+++ tests/mcp/deployment/GCP/terraform/main.tf
@@ -10,7 +10,6 @@ provider "kubernetes" {
   token                  = "dummy-token"
   cluster_ca_certificate = "dummy-cert"
   insecure               = true
-  load_config_file       = false
 }
 
 module "mcp" {
